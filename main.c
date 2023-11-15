05096cba70e7b6c89bcf6b6fc107fb87d1ae25a5
Souleimane LEVASLOT <slevaslo@bess-f1r1s1.clusters.42paris.fr>
2023-11-15 02:39:31 +0100

    affichage de la minimap?

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: slevaslo <slevaslo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 19:35:44 by slevaslo          #+#    #+#             */
-/*   Updated: 2023/11/15 02:27:09 by slevaslo         ###   ########.fr       */
+/*   Updated: 2023/11/15 02:39:08 by slevaslo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,31 @@ int	start_program(char *map_name, t_data *data)
 int	exit_all(void)
 {
 	exit(0);
+}
+
+void	draw_minimap(t_data *data)
+{
+	float		x;
+	float		y;
+
+	y = 0;
+	while (data->map[(int)y] != NULL)
+	{
+		x = 0;
+		while (data->map[(int)y][(int)x] != '\0')
+		{
+			if (data->map[(int)y][(int)x] == '0') // Mur
+				mlx_pixel_put(data->mlx_ptr, data->win_ptr, x * 20, y * 20, 0xFFFFFF); // Blanc
+			else if (data->map[(int)y][(int)x] == 'N' || data->map[(int)y][(int)x] == 'S' || data->map[(int)y][(int)x] == 'E' || data->map[(int)y][(int)x] == 'W') // Joueur
+				mlx_pixel_put(data->mlx_ptr, data->win_ptr, x * 20, y * 20, 0xFF0000); // Rouge
+			else if (data->map[(int)y][(int)x] == '1') // Objet
+				mlx_pixel_put(data->mlx_ptr, data->win_ptr, x * 20, y * 20, 0x00FF00); // Vert
+			else
+				mlx_pixel_put(data->mlx_ptr, data->win_ptr, x * 20, y * 20, 0x000000); // Noir (espace vide)
+			x += 0.1;
+		}
+		y += 0.1;
+	}
 }
 
 
@@ -58,6 +83,7 @@ int	main(int ac, char **av)
 			return (1);
 		if (start_program(av[1], &data) < 0)
 			return (exit_all());
+		draw_minimap(&data);
 		mlx_key_hook(data.win_ptr, del_key, &data);
 		mlx_hook(data.win_ptr, 17, 0, close_window, &data);
 		mlx_loop(data.mlx_ptr);
